4f29ae8c2317abe6097b91123db75df23c218d61
SAMPENSION\cas <cas@sampension.dk>
2022-05-16 15:45:49 +0200

    Adding first module

---
+++ Modules/resourcegroupandstorage/storage.tf
@@ -0,0 +1,21 @@
+resource "azurerm_resource_group" "resource_group" {
+  name     = var.resource_group_name
+  location = var.location
+  
+  tags = {
+    environment = var.tag_environment
+  }
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storageaccount_name
+  resource_group_name      = azurerm_resource_group.resource_group.name
+  location                 = azurerm_resource_group.resource_group.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = var.tag_environment
+    managed-by = "terraform"
+  }
+}
---
+++ Modules/resourcegroupandstorage/variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  type    = string
+}
+
+variable "resource_group_name" {
+  type    = string
+}
+
+variable "storageaccount_name" {
+  type    = string
+}
+
+variable "tag_environment" {
+  type    = string
+}
